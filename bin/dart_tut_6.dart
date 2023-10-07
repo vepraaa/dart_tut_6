534a3e3148cd576494767739e49f7d3cc72fbc2b
vepraaa <a.veprikovaa@yandex.ru>
2023-10-07 09:45:45 +0500

    first commit

---
+++ bin/dart_tut_6.dart
@@ -0,0 +1,5 @@
+import 'package:dart_tut_6/dart_tut_6.dart' as dart_tut_6;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_tut_6.calculate()}!');
+}
---
+++ test/dart_tut_6_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_tut_6/dart_tut_6.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
